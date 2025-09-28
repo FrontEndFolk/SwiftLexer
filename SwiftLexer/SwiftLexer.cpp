0f41055246ba2648e2863f6928307b697e32749e
vladimir.ulchenko <vovaulchenko@gmail.com>
2025-09-28 14:00:37 +0300

    initial commit

---
+++ SwiftLexer/SwiftLexer.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "FlexLexer.h"
+
+int main() {
+    yyFlexLexer lexer;
+    while (lexer.yylex() != 0) {
+        // токены обрабатываются внутри lex.yy.cc
+    }
+    return 0;
+}
