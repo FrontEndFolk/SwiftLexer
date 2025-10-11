b0092aa89eafb7dcf78854a1ca9a254ab3c1bf33
vladimir.ulchenko <vovaulchenko@gmail.com>
2025-10-11 12:03:24 +0300

    Автоматизация сборки и вывода результата лексера

--- SwiftLexer/SwiftLexer.cpp
+++ SwiftLexer/SwiftLexer.cpp
@@ -1,10 +1,23 @@
-﻿#include <iostream>
-#include "FlexLexer.h"
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // Для FILE, fopen(), perror()
+#include <stdlib.h>  // Для exit(), EXIT_FAILURE и т.п.
+#include <iostream>
 
-int main() {
-    yyFlexLexer lexer;
-    while (lexer.yylex() != 0) {
-        // токены обрабатываются внутри lex.yy.cc
+using namespace std;
+
+extern FILE* yyin;   // Определено в сгенерированном lex.yy.c
+int yylex(void);     // Главная функция лексера
+
+int main(int argc, char** argv)
+{
+    if (argc > 1) {
+        FILE* fp = fopen(argv[1], "r");
+        if (!fp) {
+            perror("Error opening file");
+            return 1;
+        }
+        yyin = fp; // для flex
     }
+    yylex();
     return 0;
 }
